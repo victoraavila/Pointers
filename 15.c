617dc363bd7817f2388b69e86989f0dc2c30a152
victor <victoraavila200@gmail.com>
2021-07-09 15:19:56 -0300

    Pointer and multi-dimensional arrays - Part 1

--- 15.c
+++ 15.c
@@ -46,6 +46,45 @@ int main(int argc, char *argv[]){
     printf("\nThe address of the first array of the matrix is %p (or %p).\n", B, &B[0]);
     printf("The address of the first element of the first array of the matrix is %p (or %p) (or %p).\n", *B, B[0], &B[0][0]);
 
+    // Filling in some values
+
+    B[0][0] = 2; B[0][1] = 3; B[0][2] = 6;
+    B[1][0] = 4; B[1][1] = 5; B[1][2] = 8;
+
+    // In a 2D array, the address of a block (a row) is the first byte in the block
+    // In this case, the base address of the second array is 12 bytes after the first array (4 bytes x 3 int)
+    // *(B + 1) is a pointer to the first integer in the second block
+
+    printf("\nThe address of the second array of the matrix is %p (or %p).\n", B + 1, &B[1]);
+    printf("\nThe next ones return pointers to integer:\n");
+    printf("The address of the first element of the second array of the matrix is %p (or %p) (or %p).\n", *(B + 1), B[1], &B[1][0]);
+    printf("The address of the third element of the second array of the matrix is %p (or %p) (or %p).\n", *(B + 1) + 2, B[1] + 2, &B[1][2]);
+
+    // Printing some integer values
+    // *B = B[0]
+    // *B + 1 = &B[0][1]
+
+    printf("\nThe second element of the array is %d.\n", *(*B + 1));
+
+    // Some equivalencies
+    // B[i][j] = *(B[i] + j) = *(*(B + i) + j)
+    // B[i] returns a pointer to the first integer in the B[i] 1D array
+
+    // Printing all elements of this matrix
+
+    printf("\nThe B matrix has %ld elements and %ld columns.\n", sizeof(B)/sizeof(int), sizeof(B[0])/sizeof(int));
+    printf("Therefore, we conclude that the B matrix has %ld rows.\n", (sizeof(B)/sizeof(int)) / (sizeof(B[0])/sizeof(int)) );
+
+    printf("\n===== PRINTING ALL MATRIX ELEMENTS =====\n");
+    for (int i = 0; i < (sizeof(B)/sizeof(int)) / (sizeof(B[0])/sizeof(int)); i++){
+        
+        for (int j = 0; j < sizeof(B[0])/sizeof(int); j++){
+
+            printf("B[%d][%d] = %d (or %d) (or %d).\n", i, j, B[i][j], *(B[i] + j), *(*(B + i) + j));
+
+        }
+
+    }
 
     return 0;
 
